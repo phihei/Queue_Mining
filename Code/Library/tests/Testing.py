d582eda2f4f1b5c37b525865e8cecd9a7626a66b
Tim Alan Stanglow <tim.stanglow@rwth-aachen.de>
2021-05-26 10:03:21 +0200

    Slight changes to comment, and added empty Tests.

---
+++ Code/Library/tests/Testing.py
@@ -0,0 +1,15 @@
+import unittest
+
+class TestLibrary(unittest.TestCase):
+    def test_analyzeServiceDiscipline(self):
+        pass
+
+    def test_analyzeServiceTimes(self):
+        pass
+
+    def test_analysisQueueArrivalRate(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
