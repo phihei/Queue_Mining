00b12c7284e5137f7ba9f95c2f0ecbf6f83a68f0
Tim Alan Stanglow <tim.stanglow@rwth-aachen.de>
2021-06-03 09:49:01 +0200

    finished analysisQueueArrivalRate for now.

--- Code/Library/src/queuemining4pm4py/functions.py
+++ Code/Library/src/queuemining4pm4py/functions.py
@@ -56,7 +56,7 @@ def analysisQueueArrivalRate(eventLog, eventList, interval, startTime=None, endT
     :type eventList: list
     :param interval: Specifies the size of the intervals in which the Queue arrivals are seperated. Use a
     datetime.timedelta object for all short timespans, and a string consisting of a (optional) (Standart: 1) number(integer)
-    and unit (either month or year) for longer.
+    and unit (either month or year) for longer timespans.
     Note: If an edge land on a day that doesn't exist (like a 29th of non leap year feburary or 31st of april, at that point
     the edge will move forward to the 1st of the next month, (and stay on the first of that month)
     :type interval: datetime.timedelta or str
@@ -64,13 +64,20 @@ def analysisQueueArrivalRate(eventLog, eventList, interval, startTime=None, endT
     :type startTime: datetime.datetime
     :param endTime: Only Counts events before StartTime. If not specified, this will be the time of the latest event in the event log.
     :type endTime: datetime.datetime
+    :param cycle: Combines diffrent repeating timeframes. Best example would be days in a week.
+    Needs to be a multiple of the interval. Use a    datetime.timedelta object for all short timespans,
+    and a string consisting of a (optional) (Standart: 1) number(integer) and unit (either month or year) for longer timespans.
+    Cycle and interval must be of the same type.
+    :type datetime.timedelta or str
     :param aligned: Specifies if the intervaledges should be aligned to higher units of time.
     Note: for this to do anything, the interval must perfectly divide a higher unit of time.
     Note: intervals specified by a datetime.timedelta, this will only align up to days, not weeks, months or years
     As example an interval of 1 hour would always have the seperation at minute 0, instead of the minute specified in starttime.
     :type aligned: bool
-    :returns: TODO find good way to represent this
-    :rtype:
+    :returns: A List of intervals, and how often events occured. This is the form of a list of tuples.
+    Each tuple contains as it's first element a tuple that defines the times in which an event had to occur to be counted. and the sencond value is the number of counts.
+    For cycles these are the times of the first occurence.
+    :rtype: list
     """
 
     if eventLog is None:
@@ -87,10 +94,37 @@ def analysisQueueArrivalRate(eventLog, eventList, interval, startTime=None, endT
     mode = 0
     if type(interval) != datetime.timedelta:
         if type(interval) == str:
-            intervalnumber, intervaltype = parseInterval(interval)
+            intervalNumber, intervalType = parseInterval(interval)
             mode = 1
         else:
-            raise TypeError("The tyoe of interval must be a string or datetime.timedelta")
+            raise TypeError("The type of interval must be a string or datetime.timedelta")
+
+    cycleMode = 0
+    intervalsinCycle = 0
+    if cycle is not None:
+        if type(cycle) != datetime.timedelta:
+            if type(cycle) == str:
+                cycleNumber, cycleType = parseCycle(cycle)
+                cycleMode = 1
+                if mode == 0:
+                    raise TypeError("The type of cycle and intervall must be the same")
+                else:
+                    if cycleType == "week":
+                        raise ValueError("The cycle must be a multiple of the interval")
+                    cycleMonths = cycleNumber if cycleType == "month" else cycleNumber * 12
+                    intervalMonths = intervalNumber if intervalType == "month" else intervalNumber * 12
+                    if cycleMonths % intervalMonths != 0:
+                        raise ValueError("The cycle must be a multiple of the interval")
+                    intervalsinCycle = cycleMonths // intervalMonths
+            else:
+                raise TypeError("The type of cycle must be a string or datetime.timedelta")
+        else:
+            if type(interval) != datetime.timedelta:
+                raise TypeError("The type of cycle and intervall must be the same")
+            if interval % cycle != 0:
+                raise ValueError("The cycle must be a multiple of the interval")
+            intervalsinCycle = interval // cycle
+
 
     if startTime is None:
         starttime = firstEventTime(eventLog)
@@ -107,45 +141,52 @@ def analysisQueueArrivalRate(eventLog, eventList, interval, startTime=None, endT
 
     relevantEvents.sort(key=lambda event: event['time:timestamp'])
     values = []
-    if cycle is None:
-        if aligned:
-            if mode == 0:
-                intervalMicroseconds = interval.microseconds + 1000000 * (interval.seconds + interval.days * 3600 * 24)
-                originalMicroseconds = starttime.microsecond + 1000000 * (starttime.second + 60 * (starttime.minute + 60 * starttime.hour))
-                if (1000000 * 3600 * 24) % intervalMicroseconds == 0:
-                    t = starttime - datetime.timedelta(microseconds=originalMicroseconds % intervalMicroseconds)
-                else:
-                    t = starttime
+    if aligned:
+        if mode == 0:
+            intervalMicroseconds = interval.microseconds + 1000000 * (interval.seconds + interval.days * 3600 * 24)
+            originalMicroseconds = starttime.microsecond + 1000000 * (
+                        starttime.second + 60 * (starttime.minute + 60 * starttime.hour))
+            if (1000000 * 3600 * 24) % intervalMicroseconds == 0:
+                t = starttime - datetime.timedelta(microseconds=originalMicroseconds % intervalMicroseconds)
             else:
-                t = starttime.replace(day=1, hour=0, minute=0, second=0, microsecond=0)
-            pass
+                t = starttime
         else:
-            t = starttime
-        i = 0
-        while t < endTime:
-            if mode == 0:
-                nt = t + interval
+            t = starttime.replace(day=1, hour=0, minute=0, second=0, microsecond=0)
+        pass
+    else:
+        t = starttime
+    i = 0
+    while t < endTime:
+        if mode == 0:
+            nt = t + interval
+        else:
+            nt = t
+            if intervalType == "month":
+                n = nt.month + intervalNumber
+                try:
+                    nt = nt.replace(year=nt.year + (n + 1 // 12), month=n % 12 if n % 12 != 0 else 12)
+                except ValueError:
+                    n = n + 1
+                    nt = nt.replace(year=nt.year + (n + 1 // 12), month=n % 12 if n % 12 != 0 else 12, day=1)
             else:
-                nt = t
-                if intervaltype == "month":
-                    n = nt.month + intervalnumber
-                    try:
-                        nt = nt.replace(year=nt.year + (n + 1 // 12), month=n % 12 if n % 12 != 0 else 12)
-                    except ValueError:
-                        n = n + 1
-                        nt = nt.replace(year=nt.year + (n + 1 // 12), month=n % 12 if n % 12 != 0 else 12, day=1)
-                else:
-                    try:
-                        nt = nt.replace(year=nt.year + intervalnumber)
-                    except ValueError:
-                        nt = nt.replace(year=nt.year + intervalnumber, month=3, day=1)
+                try:
+                    nt = nt.replace(year=nt.year + intervalNumber)
+                except ValueError:
+                    nt = nt.replace(year=nt.year + intervalNumber, month=3, day=1)
+        if cycle is None or i < intervalsinCycle:
             te = (t, nt)
-            t = nt
             values.append((te, 0))
-            while relevantEvents and relevantEvents[0]['time:timestamp'] < t:
-                values[i] = (values[i][0], values[i][1] + 1)
-                relevantEvents.pop(0)
-            i = i + 1
+            if cycle is None:
+                ri = i
+            else:
+                ri = i % intervalsinCycle
+        else:
+            ri = i
+        t = nt
+        while relevantEvents and relevantEvents[0]['time:timestamp'] < t:
+            values[ri] = (values[ri][0], values[ri][1] + 1)
+            relevantEvents.pop(0)
+        i = i + 1
 
     return values
 
@@ -164,6 +205,22 @@ def parseInterval(interval):
     raise ValueError("Interval String incorrect")
 
 
+def parseCycle(cycle):
+    t = cycle.lstrip("0123456789")
+    n = cycle[:-len(t)]
+    if n == "":
+        n = 1
+    else:
+        n = int(n)
+    if t[:4] == "week":
+        return n, "week"
+    if t[:5] == "month":
+        return n, "month"
+    if t[:4] == "year":
+        return n, "year"
+    raise ValueError("Cylce String incorrect")
+
+
 def firstEventTime(eventLog):
     earliestTime = datetime.datetime(2025, 1, 1, tzinfo=eventLog[0][0]["time:timestamp"].tzinfo)
     for trace in eventLog:
@@ -180,22 +237,3 @@ def lastEventTime(eventLog):
             if lastTime < event["time:timestamp"]:
                 lastTime = event["time:timestamp"]
     return lastTime
-
-
-def convertInterval(interval, intervalUnits):
-    if intervalUnits == "days":
-        return datetime.timedelta(days=interval)
-    if intervalUnits == "seconds":
-        return datetime.timedelta(seconds=interval)
-    if intervalUnits == "microseconds":
-        return datetime.timedelta(microseconds=interval)
-    if intervalUnits == "milliseconds":
-        return datetime.timedelta(milliseconds=interval)
-    if intervalUnits == "minutes":
-        return datetime.timedelta(minutes=interval)
-    if intervalUnits == "hours":
-        return datetime.timedelta(hours=interval)
-    if intervalUnits == "weeks":
-        return datetime.timedelta(weeks=interval)
-    raise ValueError(
-        "IntervalUnits are not an accepted IntervalUnits (microseconds, milliseconds, seconds, minutes, hours, days or weeks")
