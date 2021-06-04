19254181400a9a6ed98dc0cb0f1455115a7f0348
Tim Alan Stanglow <tim.stanglow@rwth-aachen.de>
2021-06-04 15:19:35 +0200

    Deleteed deprecated analyzeServiceTimes, and imporved analyzeServiceDiscipline docstring.

--- Code/Library/src/queuemining4pm4py/__init__.py
+++ Code/Library/src/queuemining4pm4py/__init__.py
@@ -1,5 +1,4 @@
 from .analyzeQueueArrivalRate import analyzeQueueArrivalRate
 from .analyzeServiceDiscipline import analyzeServiceDiscipline
-from .analyzeServiceTimes import analyzeServiceTimes
 from .statistics_logs import *
 from .xes_to_nx_utilities import *
--- Code/Library/src/queuemining4pm4py/analyzeServiceDiscipline.py
+++ Code/Library/src/queuemining4pm4py/analyzeServiceDiscipline.py
@@ -4,17 +4,18 @@ def analyzeServiceDiscipline(eventLog, queueDict):
     Analyzes the service discipline of events in an event Log.
 
     :param eventLog: A PM4PY event log
-    :type eventLog: str
+    :type eventLog: pm4py.objects.log.obj.EventLog
     :param queueDict: A dictionary. The values of this dictionary are 2-Tuples,
-    whos values are either strings or lists of strings, corresponding to
-    activities from the event log. The first value of the tuple corresponds to events
-    after which the case is available for servicing, the 2nd value of the tuple
-    corresponds to events that signal that the case has begun servicing.
+        whos values are either strings or lists of strings, corresponding to
+        activities from the event log. The first value of the tuple corresponds to events
+        after which the case is available for servicing, the 2nd value of the tuple
+        corresponds to events that signal that the case has begun servicing.
+
     :type queueDict: dict
     :returns: A dictionary with the same keys as queueDict, with the values being
-    n-Tuples of floats that correspond to percentages for adherance to diffrent
-    potential Service Disciplines (FIFO-Adherance, LIFO-Adherance, other)
-    TODO define which Service Disciplines we are testing for
+        n-Tuples of floats that correspond to percentages for adherance to diffrent
+        potential Service Disciplines (FIFO-Adherance, LIFO-Adherance, other)
+
     :rtype: tuple
     """
     # TODO implemtation
